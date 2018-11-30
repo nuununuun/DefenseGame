8d1d17cb6fcd63b7ea3ef8c9f8df0c937c12c154
nuununuun <triplecode@kookmin.ac.kr>
2018-12-01 05:34:53 +0900

    no message

--- Assets/Scripts/MapManager.cs
+++ Assets/Scripts/MapManager.cs
@@ -5,16 +5,46 @@ using UnityEngine;
 public class MapManager : MonoBehaviour {
     public static MapManager Instance;
 
-    public int width;
-    public int height;
+    public Transform enemyEnterPoint;
+    public Transform enemyExitPoint;
 
-	// Use this for initialization
-	void Awake () {
+    public int width = 17;
+    public int height = 13;
+
+    public PF.Grid grid { get; private set; }
+
+    int[,] map;
+    bool[,] walkableMap;
+
+    void Awake() {
         if (Instance == null) Instance = this;
-	}
 
-    // Update is called once per frame
-    void Update () {
-		
-	}
+        map = new int[height, width];
+        walkableMap = new bool[height, width];
+
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                walkableMap[i, j] = (map[i, j] == 0);
+            }
+        }
+
+        grid = new PF.Grid(walkableMap);
+    }
+
+    void UpdateGridMap() {
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                walkableMap[i, j] = (map[i, j] == 0);
+            }
+        }
+        grid.UpdateGrid(walkableMap);
+    }
+
+    public Vector2 GridToWorldPoint(Vector2Int gp) {
+        return (gp - (Vector2)transform.position) - new Vector2(width / 2, height / 2);
+    }
+
+    public Vector2Int WorldToGridPoint(Vector2 wp) {
+        return Vector2Int.FloorToInt(wp - (Vector2)transform.position) + new Vector2Int(width / 2, height / 2);
+    }
 }
---
+++ Assets/Scripts/Unit/UnitController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitController : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+		PF.Pathfinding.FindPath(MapManager.Instance.grid, MapManager.Instance.)
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
