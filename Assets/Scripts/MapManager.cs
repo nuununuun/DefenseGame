e63b9377db4a94781c27929658781acd0acfe120
nuununuun <triplecode@kookmin.ac.kr>
2018-12-01 05:06:48 +0900

    맵 매니저 생성

---
+++ Assets/Scripts/MapManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapManager : MonoBehaviour {
+    public static MapManager Instance;
+
+    public int width;
+    public int height;
+
+	// Use this for initialization
+	void Awake () {
+        if (Instance == null) Instance = this;
+	}
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
