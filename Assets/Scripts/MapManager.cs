32418fcef269c19f460fcf2c0b5057e99a4d8159
lovelyoverflow <lovelyoverflow@gmail.com>
2018-12-01 05:34:34 +0900

    draw map

--- Assets/Scripts/MapManager.cs
+++ Assets/Scripts/MapManager.cs
@@ -8,11 +8,21 @@ public class MapManager : MonoBehaviour {
     public int width;
     public int height;
 
+    public GameObject tile;
 	// Use this for initialization
 	void Awake () {
         if (Instance == null) Instance = this;
 	}
 
+    void Start() {
+        for(int i=0; i<17; i++)
+        {
+            for(int j=0; j<13; j++)
+            {
+                Instantiate(tile, new Vector2((i - 8.0f) * 0.5f, (j - 6.0f) * 0.5f), Quaternion.identity);
+            }
+        }
+    }
     // Update is called once per frame
     void Update () {
 		
